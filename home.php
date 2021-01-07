339fa1b1d02a7ce7c68bcdd624a5249db322b36e
Theman49 <cakdeny49@gmail.com>
2021-01-08 01:44:09 +0700

    update

--- home.php
+++ home.php
@@ -24,6 +24,9 @@
         <?php
         	include "./maps.php";
         ?>
+        <?php
+        	include "./maps-search.php";
+        ?>
 							
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 		<script src="./script/jquery-3.5.1.min.js"></script>
